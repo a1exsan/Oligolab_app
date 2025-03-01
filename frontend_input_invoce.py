b75d0f0b660aed23dea8e87470c769200e7e3bef
alex <gominsan@gmail.com>
2025-03-01 19:20:06 +0700

    commit 32

--- frontend_input_invoce.py
+++ frontend_input_invoce.py
@@ -69,14 +69,20 @@ def get_price_tab(scale):
     price_1_3_list = [30, 600, 600, 700, 1000, 1500, 1500, 1800, 1000, 1000, 1000, 1000, 1300]
     price_3_10_list = [40, 1000, 1000, 1200, 1800, 2000, 2000, 2200, 1800, 1800, 1800, 1800, 1300]
     price_10_15_list = [45, 1700, 1700, 2300, 2700, 3000, 3000, 3200, 2700, 2700, 2700, 2700, 1300]
+    price_15_20_list = [50, 2400, 2400, 3300, 3700, 4000, 4000, 4200, 3700, 3700, 3700, 3700, 1300]
+    price_30_40_list = [80, 4000, 4000, 4300, 4700, 5000, 5000, 5200, 4700, 4700, 4700, 4700, 1300]
 
     price_df = pd.DataFrame({'Unit': unit_list})
     if scale == '1-3':
         price_df['Price, RUB'] = price_1_3_list
     elif scale == '3-10':
         price_df['Price, RUB'] = price_3_10_list
-    else:
+    elif scale == '10-15':
         price_df['Price, RUB'] = price_10_15_list
+    elif scale == '15-20':
+        price_df['Price, RUB'] = price_15_20_list
+    elif scale == '30-40':
+        price_df['Price, RUB'] = price_30_40_list
 
     price_df['Number'] = [0 for i in range(price_df.shape[0])]
     price_df['Sum, RUB'] = [0 for i in range(price_df.shape[0])]
@@ -133,7 +139,7 @@ class oligo_orders_form_layout():
                 )),
                 dbc.Col([
                 dbc.Container(html.Div([
-                    dcc.Dropdown(['1-3', '3-10', '10-15'], init_scale,
+                    dcc.Dropdown(['1-3', '3-10', '10-15', '15-20', '30-40'], init_scale,
                                  id='synth-scale-selector'),
                     html.Br(),
                     dbc.Container(self.price_tab)
--- price_data.py
+++ price_data.py
@@ -5,14 +5,20 @@ def get_price_tab(scale):
     price_1_3_list = [30, 600, 600, 700, 1000, 1500, 1500, 1800, 1000, 1000, 1000, 1000, 1300]
     price_3_10_list = [40, 1000, 1000, 1200, 1800, 2000, 2000, 2200, 1800, 1800, 1800, 1800, 1300]
     price_10_15_list = [45, 1700, 1700, 2300, 2700, 3000, 3000, 3200, 2700, 2700, 2700, 2700, 1300]
+    price_15_20_list = [50, 2400, 2400, 3300, 3700, 4000, 4000, 4200, 3700, 3700, 3700, 3700, 1300]
+    price_30_40_list = [80, 4000, 4000, 4300, 4700, 5000, 5000, 5200, 4700, 4700, 4700, 4700, 1300]
 
     price_df = pd.DataFrame({'Unit': unit_list})
     if scale == '1-3':
         price_df['Price, RUB'] = price_1_3_list
     elif scale == '3-10':
         price_df['Price, RUB'] = price_3_10_list
-    else:
+    elif scale == '10-15':
         price_df['Price, RUB'] = price_10_15_list
+    elif scale == '15-20':
+        price_df['Price, RUB'] = price_15_20_list
+    elif scale == '30-40':
+        price_df['Price, RUB'] = price_30_40_list
 
     price_df['Number'] = [0 for i in range(price_df.shape[0])]
     price_df['Sum, RUB'] = [0 for i in range(price_df.shape[0])]
